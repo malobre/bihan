e3a66defb3fa551fc53c8cfb77748bc8efe8f95a
Maël Obréjan <mael.obrejan@protonmail.com>
2026-01-10 18:31:56 +0100

    perf(filter-url-pattern): cache URLPattern

--- src/handlers/filter-url-pattern.ts
+++ src/handlers/filter-url-pattern.ts
@@ -1,6 +1,25 @@
 import type { Context } from "#core/context.ts";
 import { type Handler, NoMatch, type RouteIntrinsics } from "#core/router.ts";
 
+const cache = new Map<string | URLPatternInit, URLPattern>();
+
+const cacheGetOrInsertComputed = (
+  pattern: string | URLPatternInit,
+  callback: () => URLPattern,
+): URLPattern => {
+  const result = cache.get(pattern);
+
+  if (result) {
+    return result;
+  }
+
+  const value = callback();
+
+  cache.set(pattern, value);
+
+  return value;
+};
+
 export const filterURLPattern = <TCtxData extends RouteIntrinsics>(
   // A pathname component pattern, URLPattern or URLPatternInit
   pattern: string | URLPattern | URLPatternInit,
@@ -18,9 +37,12 @@ export const filterURLPattern = <TCtxData extends RouteIntrinsics>(
     const urlPatternResult = (
       pattern instanceof URLPattern
         ? pattern
-        : typeof pattern === "string"
-          ? new URLPattern({ pathname: pattern })
-          : new URLPattern(pattern)
+        : // TODO: replace by `Map.prototype.getOrInsert` when available on more platforms
+          cacheGetOrInsertComputed(pattern, () =>
+            typeof pattern === "string"
+              ? new URLPattern({ pathname: pattern })
+              : new URLPattern(pattern),
+          )
     ).exec(ctx.request.url);
 
     if (urlPatternResult === null) {
